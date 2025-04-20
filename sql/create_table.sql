77fc050ea6ed955a711155d6df59e4816ab2fbfb
deapt <1470290218@qq.com>
2025-04-20 21:22:44 +0800

    后端项目基本框架搭建

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,39 +1,18 @@
--- 增加一个字段
-DROP TABLE IF EXISTS `tag`;
--- auto-generated definition
-create table tag
+create table if not exists deapt.user
 (
-    id         bigint auto_increment comment 'id' primary key,
-    tagName    varchar(255)                       null comment '标签名称',
-    userId     varchar(256)                       null comment '用户 id',
-    parentId   bigint                             null comment '父标签',
-    isParent   tinyint                            null comment '0-是父标签, 1-不是父标签',
-    createTime datetime default CURRENT_TIMESTAMP null comment '创建时间',
-    updateTime datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete   tinyint  default 0                 null comment '是否删除',
-    constraint uniIndex_tagName
-        unique (tagName)
-)
-    comment '标签表';
-
-DROP TABLE IF EXISTS `user`;
--- auto-generated definition
-create table user
-(
-    id         bigint auto_increment comment 'id' primary key,
-    username     varchar(255)                       null comment '用户昵称',
+    id           bigint auto_increment comment 'id'
+        primary key,
+    username     varchar(256)                       null comment '用户昵称',
     userAccount  varchar(256)                       null comment '账号',
-    avatarUrl    varchar(1024)                      null comment '头像',
+    avatarUrl    varchar(1024)                      null comment '用户头像',
     gender       tinyint                            null comment '性别',
-    userPassword varchar(256)                       not null comment '密码',
-    phone        varchar(512)                       null comment '手机号',
+    column_name  int                                null,
+    userPassword varchar(512)                       not null comment '用户密码',
+    phone        varchar(128)                       null comment '电话',
     email        varchar(512)                       null comment '邮箱',
-    userStatus   tinyint  default 0                 null comment '是否有效',
+    usrStatus    int      default 0                 not null comment '状态0 - 正常',
     createTime   datetime default CURRENT_TIMESTAMP null comment '创建时间',
-    updateTime   datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '替换时间',
-    isDelete     tinyint  default 0                 null comment '是否删除',
-    userRole     int      default 0                 not null comment '用户角色 0-普通会员  1-管理员',
-    userCode     varchar(512)                       null comment '用户编号'
-);
-
-alter table user add COLUMN tags varchar(1024) null comment '标签列表';
+    updateTime   datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete     tinyint  default 0                 not null comment '是否删除0-否'
+)
+    comment '用户表';
